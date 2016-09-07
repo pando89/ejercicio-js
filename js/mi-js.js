a374e96f2e3f076f98f16111143d80f7182d9b5c
pando89 <lea.rub.89@gmail.com>
2016-09-07 12:42:49 -0300

    Arreglado el CSS y el js

--- js/mi-js.js
+++ js/mi-js.js
@@ -1,7 +1,7 @@
 $("document").ready(function(){
-  $('article:odd').css("color", "black");
-  $('.post:last').css("font-weight", "bold");
-  $('.post:first').css("font-weight", "bold");
+/*  $('article:odd').css("color", "black");
+   $('.post:last').css("font-weight", "bold");
+  $('.post:first').css("font-weight", "bold");*/
   setTimeout(function(){
     $('article:odd').css("color", "red");
     $('article:even').css("color", "black");
